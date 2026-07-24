d8e3cefda1191e2f5cd679312270d206ef5b9dd5
Raph049 <130841437+Raph049@users.noreply.github.com>
2026-07-24 13:12:47 +0200

    Update update_quote.py

--- update_quote.py
+++ update_quote.py
@@ -7,9 +7,14 @@ with open(".github/quote/naruto-quotes.json", "r", encoding="utf-8") as f:
 
 quote = random.choice(quotes)
 
-new_quote = f'''> "{quote["quote"]}"  
-> **— {quote["author"]}**'''
+new_quote = f'''
+"{quote["quote"]}"
 
+— {quote["author"]}
+
+STATUS  : ONLINE
+MISSION : BUILD • LEARN • SECURE
+'''
 # Read README
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
